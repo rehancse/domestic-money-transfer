d1eb83bca6c6716fb40b7e195b87b4e1a2ab9f70
unknown <rehan1910@gmail.com>
2020-09-01 21:56:14 +0530

    adding files

---
+++ js/js-2.js
@@ -0,0 +1,37 @@
+
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("menu-hide0").style.display = "inline-block";
+	document.getElementById("menu-hide1").style.display = "inline-block";
+	document.getElementById("menu-hide2").style.display = "inline-block";
+	document.getElementById("menu-hide3").style.display = "inline-block";
+	document.getElementById("menu-hide4").style.display = "inline-block";
+	document.getElementById("menu-hide5").style.display = "inline-block";
+	document.getElementById("menu-hide6").style.display = "inline-block";
+	document.getElementById("menu-hide7").style.display = "inline-block";
+	document.getElementById("menu-hide8").style.display = "inline-block";
+}
+
+
+
+
+
+
+
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("menu-hide0").style.display = "none";
+	document.getElementById("menu-hide1").style.display = "none";
+	document.getElementById("menu-hide2").style.display = "none";
+	document.getElementById("menu-hide3").style.display = "none";
+	document.getElementById("menu-hide4").style.display = "none";
+	document.getElementById("menu-hide5").style.display = "none";
+	document.getElementById("menu-hide6").style.display = "none";
+	document.getElementById("menu-hide7").style.display = "none";
+	document.getElementById("menu-hide8").style.display = "none";
+}
